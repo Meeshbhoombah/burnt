6ae5b32bcf9dde5586fa04c3f0422ede679936c0
rohanmishra2017@gmail.com <meeshbhoombah@Rohans-MacBook-Pro.local>
2018-07-08 10:09:45 -0700

    Added requirements.txt

--- burnt.py
+++ burnt.py
@@ -6,31 +6,42 @@
 import cv2
 import sys
 
+def rename(filename, type = None):
+    """Takes in a filepath and outputs the filepath with the renamed output file"""
+    output_elements = filename.split('.')
 
-GRID_X = 12
-GRID_Y = 12 # ratio of the y to the x
-# make it scaleable
+    if type is None:
+        print("-*- BUILDING OUTPUT FILEPATH -*-")
+        output_elements.insert(1, "_resized")
+        output_elements.insert(2, ".")
+        return ''.join(output_elements) 
 
 
 def main(args):
 
-    # TODO -: fix index for executeable
+    # TODO -: fix index for executeables
     filename = args[1]
+
+    # CLI arguments passed in as string
+    grid_X = int(args[2])
+    grid_Y = int(args[3])
   
     cap = cv2.VideoCapture(filename)
-    # reads first frame of video if it exists
+    # open video at filepath and attempt to read first
+    # frame of the video
     success, image = cap.read()
 
     if not success:
         print("Could not read file at given file path.")
-
-    while success:
-        resized = cv2.resize(image, (GRID_X, GRID_Y) interpolation=cv2.LINEAR)
-   
-    output_filename = filename.split('.')
-    output_filename.insert(1, "_resized")
     
-    #output = cv2.VideoWriter(
+    # file exists and could read, create output path to
+    # release resized image as a sequence
+    out_path = rename(filename)
+    out = cv2.VideoWriter(out_path)
+        
+    while success:
+        resized = cv2.resize(image, (grid_X, grid_Y), interpolation = cv2.INTER_LINEAR)
+        out.write(resized) 
 
 
 if __name__ == "__main__":
